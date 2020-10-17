4804e5e626df2de797c9cd3a04c351788cc70967
Constantine <buzuev.k@mail.ru>
2020-10-17 11:43:13 +0400

    fix(index): setup main script file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+
+import { Provider } from 'react-redux'
+import initStore, { history } from './redux/store.js'
+import { ConnectedRouter } from 'connected-react-router'
+import { PersistGate } from 'redux-persist/es/integration/react'
+
+import MainRouter from './router/MainRouter'
+import { Route } from 'react-router-dom'
+
+const { store, persistor } = initStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Route component={MainRouter} />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
