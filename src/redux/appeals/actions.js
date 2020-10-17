5d8af83eb402b58c7eef9aa1ab9dacb0aba39ae9
Constantine <buzuev.k@mail.ru>
2020-10-17 16:27:07 +0400

    feat(redux): add select appeal action

--- src/redux/appeals/actions.js
+++ src/redux/appeals/actions.js
@@ -1,8 +1,13 @@
-import { GET_APPEALS, SUBMIT_APPEAL } from '../types'
+import { GET_APPEALS, SELECT_APPEAL, SUBMIT_APPEAL } from '../types'
 
 export const getAppeals = (appeals) => ({
   type: GET_APPEALS,
   payload: appeals,
+})
+
+export const selectAppeal = (id) => ({
+  type: SELECT_APPEAL,
+  payload: id,
 })
 
 export const submitAppeal = (id) => ({
