2f683c765763516dbc9f706451f8661fb84650e6
Constantine <buzuev.k@mail.ru>
2020-10-17 16:30:46 +0400

    fix(pages): modify home page for second component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import styles from './HomePage.module.scss'
 
-export default class HomePage extends React.Component {
+import connect from 'react-redux/es/connect/connect'
+
+import AppealsList from '../../components/AppealsList/AppealsList'
+class HomePage extends React.Component {
+  static propTypes = {
+    selectedAppeal: PropTypes.string,
+  }
   render() {
-    return <div className={classNames(styles.container)}></div>
+    return (
+      <div className={classNames(styles.container)}>
+        <AppealsList />
+        {/* TODO: remove after AppealsForm created */}
+        {this.props.selectedAppeal && (
+          <div className={classNames(styles.form)}>Form be there!</div>
+        )}
+      </div>
+    )
   }
 }
+
+const mapStateToProps = ({ appealsReducer }) => ({
+  selectedAppeal: appealsReducer.selectedAppeal,
+})
+
+export default connect(mapStateToProps)(HomePage)
